2e944788f66c29e520de7a7ea8c62088c822c702
Avtar-cloud <avtar.singh.btech2024@sitpune.edu.in>
2025-08-28 01:55:19 +0530

    Create Pointers.cpp

---
+++ Pointers.cpp
@@ -0,0 +1,22 @@
+// AVTAR SINGH
+// 24070123027
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int *aptr = &a;
+    cout << a << endl;
+    cout << aptr << endl;
+    cout << *aptr << endl;
+    cout << &a << endl;
+
+    return 0;
+}
+
+// OUTPUT
+// 10
+// 0x261fe78
+// 10
+// 0x261fe78
